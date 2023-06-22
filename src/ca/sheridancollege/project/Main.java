2dfa6490d1bb40812eda217ce4d821212cc86bee
sukhpreet11s <133692977+sukhpreet11s@users.noreply.github.com>
2023-06-21 21:23:12 -0400

    Update Main.java

--- src/ca/sheridancollege/project/Main.java
+++ src/ca/sheridancollege/project/Main.java
@@ -1,5 +1,10 @@
 package ca.sheridancollege.project;
 
+/**
+ * @author Devanshi Sharma, Sukhpreet Ghuman, Khushbu Khushbu, Muskan Muskan and Tejas Chahal
+ * @date 21 June 2023
+ */
+
 public class Main {
 
     public static void main(String[] args) {
