55ecf9c6e3360031fb8e31a9c625bc32311b2106
sukhpreet11s <133692977+sukhpreet11s@users.noreply.github.com>
2023-06-21 21:23:32 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,16 +1,11 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
+
 package ca.sheridancollege.project;
 
 /**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Devanshi Sharma, Sukhpreet Ghuman, Khushbu Khushbu, Muskan Muskan and Tejas Chahal
+ * @date 21 June 2023
  */
+
 public abstract class Player {
 
     private String name; //the unique name for this player
